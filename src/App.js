6de28b8f7ff0f55f14fd1ad3b86e102220189308
beckyjeong03 <jeongsw0710@gmail.com>
2024-01-05 16:54:58 -0500

    dd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner';
-import { Skills } from './components/Skills';
-import { Project } from './components/Projects';
-import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
-import {BrowserRouter as Router} from 'react-router-dom';
-import Main from './components/Main'
+// import { Banner } from './components/Banner';
+// import { Skills } from './components/Skills';
+// import { Project } from './components/Projects';
+// import { Contact } from './components/Contact';
+// import { Footer } from './components/Footer';
+// import {BrowserRouter as Router} from 'react-router-dom';
+// import Main from './components/Main'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
